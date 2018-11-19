5b176a0afdf399291653644c255b3cc8a65b458d
Ed Lin <eddie0lin@hotmail.com>
2018-11-19 14:26:19 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,20 +1,32 @@
 <?php
-  $json_str = file_get_contents('php://input'); // 接收 request 的 body
-  $json_obj = json_decode($json_str); // 轉 json
-  $myfile = fopen("log.txt","w+") or die("unable to open file!"); // 設定一個 txt 來列印訊息
-  fwrite($myfile,"\xEF\xBB\xBF".$json_str);  // 轉utf-8
+  $json_str = file_get_contents('php://input'); //接收request的body
+  $json_obj = json_decode($json_str); //轉成json格式
+  
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  
   $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
+  $sender_txt=rawurlencode($sender_txt); //因為使用get的方式呼叫luis api，所以需要轉碼
+  $ch = curl_init('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4ca269d3-d5f6-4914-b11c-454c2295f9ab?subscription-key=d03b8116ea1e4e7099c06a5711fc9ecc&timezoneOffset=-360&q='.$sender_txt);                                                                      
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                          
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result_str = curl_exec($ch);
+  fwrite($myfile, "\xEF\xBB\xBF".$result_str); //在字串前加上\xEF\xBB\xBF轉成utf8格式
+  $result = json_decode($result_str);
+  $ans_txt = $result -> topScoringIntent -> intent;
   $response = array (
     "to" => $sender_userid,
     "messages" => array (
       array (
         "type" => "text",
-        "text" => "Hello. You say". $sender_txt
+        "text" => $ans_txt
       )
     )
   );
+  
   
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
